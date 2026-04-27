c4883e4be2fcd5d228027f1dc665b2a7730bdf29
Branson Poole <bpool58@wgu.edu>
2026-04-27 13:54:34 -0700

    Lab 2.29: greet user with input name using Scanner

---
+++ src/labs/LabProgram1.java
@@ -0,0 +1,15 @@
+package labs;
+import java.util.Scanner;
+
+
+public class LabProbram1 {
+    public static void main(String[] args) {
+    Scanner scnr = new Scanner(System.in);
+    String userName;
+
+    userName = scnr.next();
+
+    String firstName = userName;
+
+    System.out.println("Hello " + firstName + ", and welcome to CS Online!");
+}
