f22c3da9392da1bc73b58c20dbeaadd39356983b
Branson Poole <bpool58@wgu.edu>
2026-05-02 00:06:44 -0700

    3.25 LAB: Leap year

--- src/labs/LabProgram3_25.java
+++ src/labs/LabProgram3_25.java
@@ -5,7 +5,7 @@ A year in the modern Gregorian Calendar consists of 365 days. In reality, the ea
 
         1) The year must be divisible by 4
         2) If the year is a century year (1700, 1800, etc.), the year must be evenly divisible by 400; therefore, both 1700 and 1800 are not leap years
-        Some example leap years are 1600, 1712, and 2016.
+        Some exxample leap years are 1600, 1712, and 2016.
         Write a program that takes in a year and determines whether that year is a leap year.
 */
 
