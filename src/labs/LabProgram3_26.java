652c4910087acc0dfaad724ae1c32c33e248e915
Branson Poole <bpool58@wgu.edu>
2026-05-02 00:07:43 -0700

    3.26 LAB: Name format

--- src/labs/LabProgram3_26.java
+++ src/labs/LabProgram3_26.java
@@ -3,7 +3,7 @@ Write a program that reads a person's name in one of two formats:
 firstName middleName lastName or firstName lastName. The program should output the name in the
 format: lastName, firstInitial.middleInitial. If a middle name is not provided,
  output lastName, firstInitial. instead. For example,
- input "Pat Silly Doe" should produce "Doe, P.S." and input "Julia Clark" should produce "Clark, J.".
+ input "Pat Silly Doe." should produce "Doe, P.S." and input "Julia Clark" should produce "Clark, J.".
 */
 package labs;
 
