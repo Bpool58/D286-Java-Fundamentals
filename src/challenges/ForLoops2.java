1561f7278ef501384defe89e286110ab062e0601
Branson Poole <bpool58@wgu.edu>
2026-05-07 03:16:17 -0700

    Add for loop to iterate through range and print values from start to end

---
+++ src/challenges/ForLoops2.java
@@ -0,0 +1,19 @@
+package challenges;
+
+import java.util.Scanner;
+
+public class ForLoops2 {
+    public static void main (String [] args) {
+        int startNumber;
+        int lastNumber;
+        int i;
+
+        Scanner input = new Scanner(System.in);
+        startNumber = input.nextInt();
+        lastNumber = input.nextInt();
+
+        for (i = startNumber; i <= lastNumber; i++) {
+            System.out.print(i + " ");
+        }
+    }
+} //
