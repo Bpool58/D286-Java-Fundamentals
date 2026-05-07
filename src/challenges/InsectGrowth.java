ab2c9be32393861f21195e38bc974b012f0e980e
Branson Poole <bpool58@wgu.edu>
2026-05-07 01:58:29 -0700

    Implement countdown loop printing Ready, numbers, and Start

---
+++ src/challenges/InsectGrowth.java
@@ -0,0 +1,24 @@
+package challenges;
+
+import java.util.Scanner;
+
+public class InsectGrowth {
+    public static void main (String [] args) {
+        Scanner scnr = new Scanner(System.in);
+        int numInsects;
+
+        numInsects = scnr.nextInt(); // Must be >= 1
+
+        /* Your solution goes here  */
+
+
+
+
+
+
+
+
+    }
+}
+
+
