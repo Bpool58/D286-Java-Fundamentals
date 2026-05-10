228c0335c1e2df958e4f70209bd103da06d33b5c
Branson Poole <bpool58@wgu.edu>
2026-05-09 22:41:31 -0700

    Add modulo and division logic for binary output lab

---
+++ src/labs/LabProgram4_14.java
@@ -0,0 +1,16 @@
+package labs;
+
+import java.util.Scanner;
+
+public class LabProgram5_13 {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int[] userList = new int[20];   // List of numElement integers specified by the user
+        int numElements;                // Number of integers in user's list
+        // Add more variables as needed
+
+        numElements = scnr.nextInt();   // Input begins with number of integers that follow
+
+        /* Type your code here. */
+    }
+}
