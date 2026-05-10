4b0cd12b789257d6bff47e1907c64775a24b817b
Branson Poole <bpool58@wgu.edu>
2026-05-10 05:33:35 -0700

    Add string traversal and character filtering logic

---
+++ src/labs/LabProgram4_17.java
@@ -0,0 +1,27 @@
+package labs;
+
+import java.util.Scanner;
+
+public class LabProgram4_17 {
+    public static void main(String[] args) {
+
+        Scanner scnr = new Scanner(System.in);
+
+        String userString;
+        String result;
+
+        userString = scnr.nextLine();
+
+        result = "";
+
+        for (int i = 0; i < userString.length(); i++) {
+
+            char currChar = userString.charAt(i);
+
+            if (Character.isLetter(currChar)) {
+                result = result + currChar;
+            }
+        }
+        System.out.println(result);
+    }
+}
