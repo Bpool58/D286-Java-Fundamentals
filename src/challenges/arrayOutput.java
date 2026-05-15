8a718afd8ad18d64574e97963fb65cfc08c89d5e
Branson Poole <bpool58@wgu.edu>
2026-05-15 00:26:00 -0700

    Add array max value tracking challenge with loop comparison logic

---
+++ src/challenges/arrayOutput.java
@@ -0,0 +1,23 @@
+package challenges;
+
+public class arrayOutput {
+    public static void main (String [] args) {
+        final int NUM_ELEMENTS = 4;
+        int [] userVals = new int[NUM_ELEMENTS];
+        int i;
+        int maxVal;
+
+        userVals[0] = 2;
+        userVals[1] = 5;
+        userVals[2] = 8;
+        userVals[3] = 4;
+
+        maxVal = userVals[0];
+        for (i = 0; i < userVals.length; ++i) {
+            if (userVals[i] >= maxVal) {
+                maxVal = userVals[i];
+                System.out.println(maxVal);
+            }
+        }
+    }
+}
